d1a7cf4b5aef5928090e593660750fefd601f678
Bailey Franklin <baileydfranklin@gmail.com>
2023-01-23 09:00:35 -0700

    Finished Assignment

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-
-
-let userSelection = prompt("rock, paper or scissors?"); //Gets the users selection
-let computerSelection = Math.random(); //Randomly generate a number                        
+let userSelection = prompt("Rock, Paper or Scissors?");
+let computerSelection = Math.random()*100;
 
 //Conditional logic goes here to determine the computers selection
+if(computerSelection < 34){
+    computerSelection = "Rock"
+}else if(computerSelection <= 67){
+    computerSelection = "Paper"
+}else{
+    computerSelection = "Scissors"
+}
 
 //<--Code goes here-->
 
 //Function to compare the userSelection and the computerSelection to determine the winner
-let compare=function(selection1,selection2){
-    
+
+function compare(userSelection, computerSelection){
+    if(userSelection == "Rock" && computerSelection == "Scissors"){
+        return  "You won!"
+    }
+    if(userSelection == "Rock" && computerSelection == "Paper"){
+        return "You Lost..."
+    }
+    if(userSelection == "Paper" && computerSelection == "Rock"){
+        return "You won!"
+    }
+    if(userSelection == "Paper" && computerSelection == "Scissors"){
+        return "You lost..."
+    }
+    if(userSelection == "Scissors" && computerSelection == "Rock"){
+        return "You lost ..."
+    }
+    if(userSelection == "Scissors" && computerSelection == "Paper"){
+        return "You won!"
+    }
+    if(userSelection == computerSelection){
+        return "It's a tie."
+    }
 }
-console.log(compare(userSelection,computerSelection)); //call the function to compare and display result
+
+
+console.log(compare(userSelection, computerSelection))
